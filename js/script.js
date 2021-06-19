43468fdefb208047c5f23ee42c8075e0d7d9f10b
Dennis Mwaniki <damos5776@gmail.com>
2021-06-19 21:32:10 +0300

    design  description toggle

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$('#image3').click(function(){
+    $('#image3').toggle();
+    $('#p-1').toggle();
+})
+$('#p-1').click(function(){
+    $('#p-1').toggle();
+    $('#image3').toggle();
+
+})
+
+
+
+});
